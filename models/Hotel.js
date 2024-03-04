b3b6bffe8ebebbecd4b7bebacd20e1c5900035aa
Star2578 <nuttykingger@gmail.com>
2024-03-04 21:22:26 +0700

    Adjust hotelSchema to fit requirement

--- models/Hotel.js
+++ models/Hotel.js
@@ -13,29 +13,9 @@ const HotelSchema = new mongoose.Schema({
         type: String,
         require:[true,'Please add an address']
     },
-    district:{
-        type: String,
-        require:[true,'Please add an district']
-    },
-    province:{
-        type: String,
-        require:[true,'Please add an province']
-    },
-    postalcode:{
-        type: String,
-        require:[true,'Please add an postalcode'],
-        maxlength:[5,'Postal Code can not be more than 5 digits']
-    },
     tel:{
         type: String
-    },
-    region:{
-        type: String,
-        require:[true,'Please add a region']
     }
-    
-
-    
 },
 
 {toJSON:{virtuals:true},toObject:{virtuals:true} , versionKey:false}
